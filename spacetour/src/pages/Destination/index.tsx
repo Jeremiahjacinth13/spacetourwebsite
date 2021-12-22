6d8b48980e33eed096c1cc2b3e9f2818557a9cd0
Jeremiah Lena <jeremiahlena13@gmail.com>
2021-12-22 11:18:07 +0100

    commit

--- spacetour/src/pages/Destination/index.tsx
+++ spacetour/src/pages/Destination/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-
+import moon from '../../assets/destination/image-moon.png'
 import './destination.css'
 
 const Destination = () => {
   return (
-    <motion.main 
-      className='destination'
-      initial = {{opacity: 0}}
-      animate = {{opacity: 1}}
-      exit = {{opacity: 0}}
+    <motion.main
+      className="destination"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
-
+      <div className="container">
+        <div>
+          <p className="subheading-1">
+            <span className="number">01</span>
+            PICK YOUR DESTINATION
+          </p>
+          <img src={moon} alt="" />
+        </div>
+        <div>
+          {/* <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sit
+          magnam, ullam minus velit ducimus necessitatibus, dolorem rerum
+          voluptatem fuga beatae. Ea odit maxime magni earum asperiores quam
+          accusantium dolorem inventore, rem ducimus quod velit excepturi, quo
+          sapiente, aspernatur nam? Assumenda libero incidunt, natus fuga quas
+          facere animi distinctio aliquam?
+          </p> */}
+        </div>
+      </div>
     </motion.main>
   )
 }
--- spacetour/src/pages/Homepage/index.tsx
+++ spacetour/src/pages/Homepage/index.tsx
@@ -15,16 +15,35 @@ const HomePage: React.FC = () => {
     >
       <div className="container">
         <div className="container__text">
-          <p className="subheading-1">SO, YOU WANT TO TRAVEL TO</p>
+          <motion.p
+            className="subheading-1"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{delay: 0.5}}
+          >
+            SO, YOU WANT TO TRAVEL TO
+          </motion.p>
           <div className="animate-down">
-            <h1 className="heading heading-1">SPACE</h1>
+            <motion.h1 
+                className="heading heading-1"
+                initial = {{opacity: 0, y: 150}}
+                animate = {{opacity: 1, y: 0}}
+                exit = {{opacity: 0, y: 150}}
+                transition={{duration: 0.8}}
+            >SPACE</motion.h1>
           </div>
-          <p>
+          <motion.p
+            initial = {{opacity: 0, y: 50}}
+            animate = {{opacity: 1, y: 0}}
+            exit = {{x: -50, opacity: 0}}
+            transition={{delay: 0.1}}
+          >
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
             world experience!
-          </p>
+          </motion.p>
         </div>
         <div className="container__button">
           <div className="main__button__container">
