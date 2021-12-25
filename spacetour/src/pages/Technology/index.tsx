63eeb098430464f5f3f198d003a677b61498c15a
Jeremiah Lena <jeremiahlena13@gmail.com>
2021-12-25 14:59:59 +0100

    na here i dey ooo

--- spacetour/src/pages/Technology/index.tsx
+++ spacetour/src/pages/Technology/index.tsx
@@ -1,7 +1,18 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+import './tech.css'
 
 const Technology = () => {
-  return <h1>This is the technology page</h1>
+  return (
+    <motion.main
+      className="tech"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+
+    </motion.main>
+  )
 }
 
 export { Technology }
